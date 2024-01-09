a693e4f20635830506f92a71abca9a17e248e490
Guilherme Bezerra <gbsantos.it@gmail.com>
2024-01-09 16:02:56 -0300

    🍱 styles: using StyleSheet

--- react-native/01-imhere/App.tsx
+++ react-native/01-imhere/App.tsx
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Text style={styles.title}>
+        Nome do evento
+      </Text>
+
+      <Text style={styles.text}>
+        Sexta, 12 de Janeiro de 2024.
+      </Text>
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#131016',
+    padding: 24
   },
+  title: {
+    color: '#FDFCFE',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48
+  },
+  text: {
+    color: '#6B6B6B',
+    fontSize: 16,
+  }
 });
