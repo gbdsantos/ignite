1f495694fdca8ace78ce085df578675731185bb4
Guilherme Bezerra <gbsantos.it@gmail.com>
2024-01-09 19:56:28 -0300

    🎨 styles: styling the StatusBar

--- react-native/01-imhere/App.tsx
+++ react-native/01-imhere/App.tsx
@@ -1,12 +1,17 @@
-import { View } from 'react-native';
+import { StatusBar, View } from 'react-native';
 
 import Home from './src/screens/Home';
 
 export default function App() {
   return (
-    <View style={{ flex: 1 }}>
+    <>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       <Home />
-    </View >
+    </>
   );
 }
 
