edb37a4af9e5b3a61cf4cea149ec340d6624ee75
Guilherme Bezerra <gbsantos.it@gmail.com>
2024-01-14 19:24:23 -0300

    📝 update: writable and transform stream

--- nodejs/01-nodejs-fundamentals/streams/fundamentals.js
+++ nodejs/01-nodejs-fundamentals/streams/fundamentals.js
@@ -16,7 +16,12 @@
 // process.stdin
 //   .pipe(process.stdout)
 
-import { Readable } from 'node:stream';
+import {
+  Duplex,
+  Readable,
+  Transform,
+  Writable
+} from 'node:stream';
 
 class OneToHundredStream extends Readable {
   index = 1;
@@ -36,4 +41,21 @@ class OneToHundredStream extends Readable {
   }
 }
 
-new OneToHundredStream().pipe(process.stdout);
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1;
+
+    callback(null, Buffer.from(String(transformed)));
+  }
+}
+
+class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10);
+    callback();
+  }
+}
+
+new OneToHundredStream()
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplyByTenStream());
