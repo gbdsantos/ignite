42f38b08f0a074cf8a9b98eb0aafb5995501553f
Guilherme Bezerra <gbsantos.it@gmail.com>
2024-01-14 20:54:22 -0300

    🗃️  database: persisting database

--- nodejs/01-nodejs-fundamentals/src/database.js
+++ nodejs/01-nodejs-fundamentals/src/database.js
@@ -1,12 +1,36 @@
+import fs from 'node:fs/promises';
+
+// console.log(import.meta.url);
+
+const databasePath = new URL('database/database.json', import.meta.url);
+
+// console.log(databasePath);
+
 export class Database {
   #database = {}
 
+  constructor() {
+    fs.readFile(databasePath, 'utf-8')
+      .then(data => {
+        this.#database = JSON.parse(data);
+      })
+      .catch(() => {
+        this.#persist();
+      });
+  }
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database));
+  }
+
   insert(table, data) {
-    if (Array.isArray(this.database[table])) {
+    if (Array.isArray(this.#database[table])) {
       this.#database[table].push(data);
     } else {
       this.#database[table] = [data];
     }
+
+    this.#persist();
 
     return data;
   }
