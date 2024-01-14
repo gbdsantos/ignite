0ae98142ac6113f174ce615bee32488c3e189ff2
Guilherme Bezerra <gbsantos.it@gmail.com>
2024-01-14 19:00:05 -0300

    📝 update: creating a readable stream

--- nodejs/01-nodejs-fundamentals/streams/fundamentals.js
+++ nodejs/01-nodejs-fundamentals/streams/fundamentals.js
@@ -7,3 +7,33 @@
 // 10mb/s - 100s (1024 / 10)
 
 // 100s -> Insertions on database
+
+// 10mb/s -> 10.000 lines
+
+// Readable Streams / Writable Streams
+
+// Streams ->
+// process.stdin
+//   .pipe(process.stdout)
+
+import { Readable } from 'node:stream';
+
+class OneToHundredStream extends Readable {
+  index = 1;
+
+  _read() {
+    const i = this.index++;
+
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null);
+      } else {
+        const buffer = Buffer.from(String(i));
+
+        this.push(buffer);
+      }
+    }, 1000);
+  }
+}
+
+new OneToHundredStream().pipe(process.stdout);
