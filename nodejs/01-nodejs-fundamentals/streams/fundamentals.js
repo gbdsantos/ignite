bc7a5a426f89cbb65a3d569969bce6e64c390db1
Guilherme Bezerra <gbsantos.it@gmail.com>
2024-01-14 18:49:24 -0300

    📝 update: knowing streams in node

---
+++ nodejs/01-nodejs-fundamentals/streams/fundamentals.js
@@ -0,0 +1,9 @@
+// Netflix & Spotify
+
+// Importing customers via CSV (Excel)
+// 1GB - 1.000.000 lines
+// POST /upload import.csv
+
+// 10mb/s - 100s (1024 / 10)
+
+// 100s -> Insertions on database
