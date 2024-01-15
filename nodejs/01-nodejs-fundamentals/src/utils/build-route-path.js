bfefb30bf63b13a4043dc3d7eccfd3fdcb79ef22
Guilherme Bezerra <gbsantos.it@gmail.com>
2024-01-15 02:27:24 -0300

    ✨ feat: capturing query parameters

--- nodejs/01-nodejs-fundamentals/src/utils/build-route-path.js
+++ nodejs/01-nodejs-fundamentals/src/utils/build-route-path.js
@@ -5,7 +5,7 @@ export function buildRoutePath(path) {
 
   // console.log(pathWithParams);
 
-  const pathRegex = new RegExp(`^${pathWithParams}`);
+  const pathRegex = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`);
 
   // console.log(Array.from(path.matchAll(routeParametersRegex)));
 
