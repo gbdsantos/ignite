c7355d98b8869fe6d2dc7960cec339e9f01eb193
Guilherme Bezerra <gbsantos.it@gmail.com>
2024-01-26 03:26:59 -0300

    🧪 test(backend): testing transactions listing

--- nodejs/02-creating-rest-api-with-nodejs/test/transactions.test.ts
+++ nodejs/02-creating-rest-api-with-nodejs/test/transactions.test.ts
@@ -1,4 +1,4 @@
-import { afterAll, beforeAll, describe, expect, test } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it, test } from 'vitest'
 import request from 'supertest'
 
 import { app } from '../src/app'
@@ -23,5 +23,29 @@ describe('Transactions routes', () => {
       .expect(201)
 
     // expect(response.statusCode).toEqual(201)
+  })
+
+  it('should be abe to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    ])
   })
 })
