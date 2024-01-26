4193cd920ece26657a00f321a6c5da3bb2fd3c96
Guilherme Bezerra <gbsantos.it@gmail.com>
2024-01-25 23:00:49 -0300

    📝 update(backend): fastify plugins

--- nodejs/02-creating-rest-api-with-nodejs/src/server.ts
+++ nodejs/02-creating-rest-api-with-nodejs/src/server.ts
@@ -1,18 +1,11 @@
-import { randomUUID } from 'crypto'
 import fastify from 'fastify'
 
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const transaction = await knex('transactions')
-    .where('amount', 1000)
-    .select('*')
-
-  return transaction
-})
+app.register(transactionsRoutes)
 
 app
   .listen({
