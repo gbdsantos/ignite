8a8f0060d99dcfffd41573807ca4e50d504108b2
Guilherme Bezerra <gbsantos.it@gmail.com>
2024-04-30 23:49:00 -0300

    🧪 test(backend): tests and authentication controller

--- nodejs/03-api-solid/src/http/routes.ts
+++ nodejs/03-api-solid/src/http/routes.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance } from 'fastify'
 
+import { authenticate } from './controllers/autenticate.controller'
 import { Register } from './controllers/register.controller'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', Register)
+  app.post('/sessions', authenticate)
 }
