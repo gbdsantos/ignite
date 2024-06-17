d55ac921af2f4353052de28af31797f929883c80
Guilherme Bezerra <gbsantos.it@gmail.com>
2024-06-16 23:51:49 -0300

    ♻️ refactor(backend): refactoring use cases

--- nodejs/04-clean-ddd/src/domain/forum/application/use-cases/delete-answer.ts
+++ nodejs/04-clean-ddd/src/domain/forum/application/use-cases/delete-answer.ts
@@ -1,14 +1,20 @@
-import { AnswerRepository } from '../repositories/answers-repository'
+import { Either, left, right } from '@/core/either'
+import { AnswersRepository } from '../repositories/answers-repository'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { NotAllowedError } from './errors/not-allowed-error'
 
 interface DeleteAnswerUseCaseRequest {
   authorId: string
   answerId: string
 }
 
-interface DeleteAnswerUseCaseResponse {}
+type DeleteAnswerUseCaseResponse = Either<
+  ResourceNotFoundError | NotAllowedError,
+  {}
+>
 
 export class DeleteAnswerUseCase {
-  constructor(private answerRepository: AnswerRepository) {}
+  constructor(private answerRepository: AnswersRepository) {}
 
   async execute({
     authorId,
@@ -17,15 +23,15 @@ export class DeleteAnswerUseCase {
     const answer = await this.answerRepository.findById(answerId)
 
     if (!answer) {
-      throw new Error('Answer not found.')
+      return left(new ResourceNotFoundError())
     }
 
     if (authorId !== answer.authorId.toString()) {
-      throw new Error('Not allowed.')
+      return left(new NotAllowedError())
     }
 
     await this.answerRepository.delete(answer)
 
-    return {}
+    return right({})
   }
 }
