aa86412919eb9c0e5e07d7cedbf6f4337e9f25f0
Guilherme Bezerra <gbsantos.it@gmail.com>
2024-06-21 22:35:47 -0300

    🧪 test(backend): question editing test

---
+++ nodejs/04-clean-ddd/test/repositories/in-memory-question-attachments-repository.ts
@@ -0,0 +1,16 @@
+import { QuestionAttachment } from '@/domain/forum/enterprise/entities/question-attachment'
+import { QuestionAttachmentsRepository } from '@/domain/forum/application/repositories/question-attachments-repository'
+
+export class InMemoryQuestionAttachmentsRepository
+  implements QuestionAttachmentsRepository
+{
+  public items: QuestionAttachment[] = []
+
+  async findManyByQuestionId(questionId: string) {
+    const questionAttachment = this.items.filter(
+      (item) => item.questionId.toString() === questionId,
+    )
+
+    return questionAttachment
+  }
+}
