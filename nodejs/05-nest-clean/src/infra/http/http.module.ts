8f6a3f43ccc9940a8c6eb3187c99fc7d2b36259d
Guilherme Bezerra <gbsantos.it@gmail.com>
2024-07-30 12:43:43 -0300

    📝 update(backend): listing recent questions

--- nodejs/05-nest-clean/src/infra/http/http.module.ts
+++ nodejs/05-nest-clean/src/infra/http/http.module.ts
@@ -1,11 +1,13 @@
+import { DatabaseModule } from '../database/database.module'
 import { Module } from '@nestjs/common'
 
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
 import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
-import { DatabaseModule } from '../database/database.module'
+
 import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question'
+import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
 
 @Module({
   imports: [DatabaseModule],
@@ -15,6 +17,6 @@ import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/crea
     CreateQuestionController,
     FetchRecentQuestionsController,
   ],
-  providers: [CreateQuestionUseCase],
+  providers: [CreateQuestionUseCase, FetchRecentQuestionsUseCase],
 })
 export class HttpModule {}
