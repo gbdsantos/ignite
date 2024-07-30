17f7f1510f329d92875dac4d4fe2c31585d19867
Guilherme Bezerra <gbsantos.it@gmail.com>
2024-07-30 10:44:24 -0300

    📝 update(backend): implementing QuestionsRepository

--- nodejs/05-nest-clean/src/infra/database/prisma/mappers/prisma-question-mapper.ts
+++ nodejs/05-nest-clean/src/infra/database/prisma/mappers/prisma-question-mapper.ts
@@ -1,6 +1,6 @@
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Question } from '@/domain/forum/enterprise/entities/question'
-import { Question as PrismaQuestion } from '@prisma/client'
+import { Question as PrismaQuestion, Prisma } from '@prisma/client'
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
 
 export class PrismaQuestionMapper {
@@ -19,5 +19,18 @@ export class PrismaQuestionMapper {
       },
       new UniqueEntityID(raw.id),
     )
+  }
+
+  static toPrisma(question: Question): Prisma.QuestionUncheckedCreateInput {
+    return {
+      id: question.id.toString(),
+      authorId: question.authorId.toString(),
+      bestAnswerId: question.bestAnswerId?.toString(),
+      title: question.title,
+      content: question.content,
+      slug: question.slug.value,
+      createdAt: question.createdAt,
+      updatedAt: question.updatedAt,
+    }
   }
 }
