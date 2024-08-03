953f4381be39095c4cc1be043d6f4ccc9e6763f6
Guilherme Bezerra <gbsantos.it@gmail.com>
2024-08-03 19:28:40 -0300

    📝 update(backend): creating EnvModule

--- nodejs/05-nest-clean/src/infra/app.module.ts
+++ nodejs/05-nest-clean/src/infra/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+
 import { AuthModule } from './auth/auth.module'
-import { envSchema } from './env'
+import { EnvModule } from './env/env.module'
 import { HttpModule } from './http/http.module'
+
+import { envSchema } from './env/env'
 
 @Module({
   imports: [
@@ -11,6 +14,7 @@ import { HttpModule } from './http/http.module'
       isGlobal: true,
     }),
     AuthModule,
+    EnvModule,
     HttpModule,
   ],
 })
--- nodejs/05-nest-clean/src/infra/auth/auth.module.ts
+++ nodejs/05-nest-clean/src/infra/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
-import { ConfigService } from '@nestjs/config'
 import { JwtModule } from '@nestjs/jwt'
 import { PassportModule } from '@nestjs/passport'
-import { Env } from '@/infra/env'
 import { JwtStrategy } from './jwt.strategy'
+
+import { EnvModule } from '../env/env.module'
+import { EnvService } from '../env/env.service'
 
 import { APP_GUARD } from '@nestjs/core'
 import { JwtAuthGuard } from './jwt-auth.guard'
@@ -12,11 +13,12 @@ import { JwtAuthGuard } from './jwt-auth.guard'
   imports: [
     PassportModule,
     JwtModule.registerAsync({
-      inject: [ConfigService],
+      imports: [EnvModule],
+      inject: [EnvService],
       global: true,
-      useFactory(config: ConfigService<Env, true>) {
-        const privateKey = config.get('JWT_PRIVATE_KEY', { infer: true })
-        const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true })
+      useFactory(env: EnvService) {
+        const privateKey = env.get('JWT_PRIVATE_KEY')
+        const publicKey = env.get('JWT_PUBLIC_KEY')
 
         return {
           signOptions: { algorithm: 'RS256' },
@@ -27,6 +29,7 @@ import { JwtAuthGuard } from './jwt-auth.guard'
     }),
   ],
   providers: [
+    EnvService,
     JwtStrategy,
     {
       provide: APP_GUARD,
