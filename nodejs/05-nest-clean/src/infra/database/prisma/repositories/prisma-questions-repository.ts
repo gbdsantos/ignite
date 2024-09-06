ef37e9e83204c01ab673e0224285e9f1c8a77b72
Guilherme Bezerra <gbsantos.it@gmail.com>
2024-09-05 21:49:17 -0300

    ♻️ refactor(backend): cache adjustments

--- nodejs/05-nest-clean/src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ nodejs/05-nest-clean/src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -80,7 +80,7 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
     if (cacheHit) {
       const cachedData = JSON.parse(cacheHit)
 
-      return cachedData
+      return PrismaQuestionDetailsMapper.toDomain(cachedData)
     }
 
     const question = await this.prisma.question.findUnique({
@@ -97,12 +97,12 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
       return null
     }
 
-    const questionDetails = PrismaQuestionDetailsMapper.toDomain(question)
-
     await this.cacheRepository.set(
       `question:${slug}:details`,
-      JSON.stringify(questionDetails),
+      JSON.stringify(question),
     )
+
+    const questionDetails = PrismaQuestionDetailsMapper.toDomain(question)
 
     return questionDetails
   }
